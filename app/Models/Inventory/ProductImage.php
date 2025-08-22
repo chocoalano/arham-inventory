9b4a64041a64da94bbc4bbe6110a2370abc76751
chocoalano <alangentina95@gmail.com>
2025-08-22 20:53:07 +0700

    update image activity log

--- app/Models/Inventory/ProductImage.php
+++ app/Models/Inventory/ProductImage.php
@@ -5,6 +5,7 @@ namespace App\Models\Inventory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 class ProductImage extends Model
@@ -22,5 +23,13 @@ class ProductImage extends Model
     public function product(): BelongsTo
     {
         return $this->belongsTo(Product::class);
+    }
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logFillable()                 // log semua field fillable
+            ->useLogName('produk image')          // nama log
+            ->dontSubmitEmptyLogs();        // hindari log kosong
     }
 }
